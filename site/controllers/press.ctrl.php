65fb115338cb44be2731cdd61d7d7a7876d72bcb
cyberlis <cyberlis@13901e78-2507-634d-b62a-01ba92d34a10>
2013-06-26 10:49:11 +0000

    git-svn-id: http://svn.server/luxgen.ru/trunk@22495 13901e78-2507-634d-b62a-01ba92d34a10

--- site/controllers/press.ctrl.php
+++ site/controllers/press.ctrl.php
@@ -65,6 +65,15 @@ class PressController extends Site_Controller
             $meta_tags['description'] = 'Новости — Пресса о нас';
             $this->page->meta = $meta_tags;
         }
+		$tmpyears = $this->model('news')->getList('news', array('created'));
+        if (!empty($tmpyears)) {
+            foreach($tmpyears as $year) {
+                $year = date('Y', strtotime($year['created']));
+                $years[$year] = $year;
+            }
+        }
+
+        $this->page->years = $years;
         $this->page->content = $this->renderView('list');
 //        debug::dump($years);
         $this->loadView('main',null);
